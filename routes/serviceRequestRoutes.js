05e2770c33079725eff5f51541f27bd3905a8c71
imrkale <kale44772@gmail.com>
2024-12-11 14:59:41 -0500

    payment issue fixed

--- routes/serviceRequestRoutes.js
+++ routes/serviceRequestRoutes.js
@@ -1,7 +1,20 @@
 import express from "express";
+import { hasRole, isAuthenticated } from "../utilities/middlewares/authenticationMiddleware";
+import { getAllServiceRequests } from "../data/servicerequests";
 
 const router = express.Router();
 
+router.get('/', isAuthenticated, hasRole('employee'), async (req, res) => {
+    try {
+        const requests = await getAllServiceRequests();
+        req.status(200).render('dashboards/employee-dashboard', {requests})
+    } catch(e) {
+        res.status(400).json({error: e.message});
+    }
+});
 
+router.post('/', isAuthenticated, hasRole('customer'), async(req, res) => {
+    
+})
 
 export default router;
